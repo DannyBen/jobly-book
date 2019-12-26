2e2262359ddf7991085eb9d32992b95fcf266d40
Danny Ben Shitrit <db@dannyben.com>
2019-12-26 14:03:55 +0000

    add spellcheck

--- test/test.sh
+++ test/test.sh
@@ -1,16 +1,49 @@
 #!/usr/bin/env bash
+
+header() {
+  green "\n===> $@"
+}
+
+error() {
+  red "\n===> $@"
+}
+
+red() { printf "\e[31m%b\e[0m\n" "$*"; }
+green() { printf "\e[32m%b\e[0m\n" "$*"; }
+
 # Check that the generated site does not have livereload
 # This can happen if you push the repository after the local server ran
 # locally, and was not rebuild.
-grep -lir livereload docs
-if [[ $? == 0 ]]; then
-  echo "Failed: Generated docs contain Livereload code. Rebuild please."
+header "Test: Livereload"
+if grep -lir livereload docs; then
+  error "FAIL: Livereload"
+  echo "     Generated docs contain Livereload code. Rebuild please."
   exit 1
 fi
 
-set -e
-
 # Check broken links
-sla check localhost:3000 --depth 10 --external --ignore "/do/Hello /statuses"
+header "Test: Broken Links"
+if ! (sla check localhost:3000 --depth 10 --external --ignore "/do/Hello /statuses"); then
+  error "FAIL: Broken Links"
+  exit 1
+fi
 
-echo "Completed successfully"
+# Check splling errors
+header "Test: Spellcheck"
+for f in src/*.md ; do
+  echo "$f"
+  output=$(aspell list --lang=en --personal aspell-custom-dict.txt --home-dir test < "$f")
+  if [[ $output ]]; then
+    failed=1
+    echo "$output"
+  fi
+done
+
+if [[ $failed ]]; then
+  error "FAIL: Spellcheck"
+  echo "     You have some typos."
+  echo "     If they are valid, add them to test/aspell-custom-dict.txt"
+  exit 1
+fi
+
+header "All good"
