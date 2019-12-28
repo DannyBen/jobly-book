719d3552e78cc857157f045e52d432091494eb78
Danny Ben Shitrit <db@dannyben.com>
2019-12-28 16:53:29 +0000

    update sla command

--- test/broken_links.sh
+++ test/broken_links.sh
@@ -2,7 +2,7 @@
 source "test/test_helper.bash"
 
 header "Test: Broken Links"
-if ! (sla check localhost:3000 --depth 10 --external --ignore "/do/Hello /statuses"); then
+if ! (sla localhost:3000 --depth 10 --external --ignore "/do/Hello /statuses"); then
   error "FAIL: Broken Links"
   exit 1
 fi
